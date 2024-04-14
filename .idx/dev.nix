493ab98d5473261ae7224d3312c049956154afb9
danishjoseph07 <danishjoseph07@gmail.com>
2024-04-14 03:57:21 +0000

    Remove env declaration from nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,16 +7,6 @@ packages =  [
     pkgs.git
   ];
 
-env = {
-    DB_HOST="";
-    DB_PORT="";
-    DB_USERNAME="";
-    DB_PASSWORD="";
-    DB_NAME="";
-    SYNCHRONIZE=true;
-  };
-
 services.docker.enable = true;
-
 
 }
