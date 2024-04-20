08a735d8545719d0afd9a9e764e95d8b5e190f0a
Danish Joseph <danishjoseph07@gmail.com>
2024-04-20 11:01:02 +0200

    Add @nestjs/schedule and AssetManagementModule to DataSyncModule imports

--- src/data-sync/data-sync.module.ts
+++ src/data-sync/data-sync.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AssetManagementModule } from 'src/asset-management/asset-management.module';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), AssetManagementModule],
 })
 export class DataSyncModule {}
