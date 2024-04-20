333a0bde8d0e4e6d05fa32e907c9065ee32b8d2a
Danish Joseph <danishjoseph07@gmail.com>
2024-04-20 11:00:23 +0200

    Add DataSyncModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AssetManagementModule } from './asset-management/asset-management.module';
 import { DatabaseModule } from './database.module';
+import { DataSyncModule } from './data-sync/data-sync.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { DatabaseModule } from './database.module';
     }),
     DatabaseModule,
     AssetManagementModule,
+    DataSyncModule,
   ],
   controllers: [AppController],
   providers: [AppService],
