26e43d321b42a232bcd9015d6814fe319e0cef34
Danish Joseph <danishjoseph07@gmail.com>
2026-08-09 21:44:36 +0000

    feat: add assets sync cli

--- apps/cli/src/commands/index.ts
+++ apps/cli/src/commands/index.ts
@@ -1,7 +1,12 @@
+import { AssetsSyncCommand } from './assets-sync.command';
 import { MarketSyncCommand } from './market-sync.command';
 import { CliCommand } from './command';
 
 export * from './command';
+export * from './assets-sync.command';
 export * from './market-sync.command';
 
-export const commands: CliCommand[] = [new MarketSyncCommand()];
+export const commands: CliCommand[] = [
+  new MarketSyncCommand(),
+  new AssetsSyncCommand(),
+];
